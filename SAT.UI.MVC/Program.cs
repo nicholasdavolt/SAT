0ae4acc2087f471b493df76d4441277869e5eb83
Nicholas Davolt <nicholadavolt@outlook.com>
2023-07-19 10:22:46 -0500

    Added Identity items

--- SAT.UI.MVC/Program.cs
+++ SAT.UI.MVC/Program.cs
@@ -16,8 +16,8 @@ namespace SAT.UI.MVC
                 options.UseSqlServer(connectionString));
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-            builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddEntityFrameworkStores<ApplicationDbContext>();
+            builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddRoles<IdentityRole>().AddRoleManager<RoleManager<IdentityRole>>().AddEntityFrameworkStores<ApplicationDbContext>();
+
             builder.Services.AddControllersWithViews();
 
             var app = builder.Build();
